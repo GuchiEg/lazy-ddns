dd7e99a07d9e01f90aacfd94a9c473650830bc37
GuchiEg <jamesdarcyhudson@gmail.com>
2021-07-26 22:09:33 +1000

    functional baby

--- access_dns.py
+++ access_dns.py
@@ -1,5 +1,5 @@
 import requests
-from secrets import *
+from secrets import API_TOKEN, API_ZONE_ID, WEBPAGE_NAME
 
 
 def get_dns_records():
@@ -8,4 +8,30 @@ def get_dns_records():
         "Authorization": f"Bearer {API_TOKEN}"
     }
     req = requests.get(f"https://api.cloudflare.com/client/v4/zones/{API_ZONE_ID}/dns_records/", headers=headers).json()
+    for result in req["result"]:
+        # Find the correct DNS 
+        if result["name"] == WEBPAGE_NAME:
+            return result
+    return None
+
+
+def get_ip_addr():
+    ip = requests.get('https://api.ipify.org').text
+    return ip
+
+
+def update_ip_addr(record, ip_addr):
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {API_TOKEN}"
+    }
+    update_data = {
+        "type": record["type"],
+        "name": WEBPAGE_NAME,
+        "content": ip_addr,
+        "ttl": 1
+    }
+    print(f"Updating IP Address to {ip_addr}")
+    req = requests.put(f"https://api.cloudflare.com/client/v4/zones/{API_ZONE_ID}/dns_records/{record['id']}/", 
+            headers=headers, json=update_data)
     return req
--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-import socket
-from access_dns import get_dns_records
+from access_dns import get_dns_records, get_ip_addr, update_ip_addr
 
-# ip = get('https://api.ipify.org').text
-# print('My public IP address is: {}'.format(ip))
+def main():
+    ip_addr = get_ip_addr()
+    record = get_dns_records()
+    if record["content"] == ip_addr:
+        return
+    req = update_ip_addr(record, ip_addr)
 
-print(get_dns_records())
+
+if __name__ == "__main__":
+    main()
